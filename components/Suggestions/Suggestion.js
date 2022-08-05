e032ce23cf47514b7ff73dbdbc8c337dc9f35b8b
Apungu Raymond <apunguray@gmail.com>
2022-08-06 04:05:09 +0800

    local changes

---
+++ components/Suggestions/Suggestion.js
@@ -0,0 +1,30 @@
+import { View, Text, FlatList, Dimensions } from "react-native";
+import React from "react";
+import SuggestCard from "./SuggestCard";
+
+const DataList = [{}, {}, {}, {}, {}, {}, {}, {}];
+
+const Suggestion = () => {
+  return (
+    <View>
+      <Text className="capitalize font-semibold text-base tracking-wider text-gray-800 px-5 mb-2">
+        Suggestion
+      </Text>
+      <View
+        style={{
+          width: Dimensions.get("window").width,
+        }}
+      >
+        <FlatList
+          data={DataList}
+          horizontal
+          keyExtractor={(_, index) => index.toString()}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <SuggestCard />}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Suggestion;
