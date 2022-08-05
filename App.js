7c8311ffbb33c7b0dbb85a38188ded5d581142b8
Apungu Raymond <apunguray@gmail.com>
2022-08-05 21:35:36 +0800

    local update

--- App.js
+++ App.js
@@ -3,12 +3,27 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TailwindProvider } from "tailwindcss-react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { HomeScreen, Onboarding } from "./screens";
+import { HomeScreen, Onboarding, Preference } from "./screens";
+import React from "react";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const initialLuanch = async () => {
+    const firstLaunch = await AsyncStorage.getItem("firstLaunch");
+    if (!firstLaunch) {
+      await AsyncStorage.setItem("firstLaunch", "true");
+      return true;
+    }
+    return false;
+  };
+
+  React.useEffect(() => {
+    initialLuanch();
+  }, []);
+
   return (
     <NavigationContainer>
       <TailwindProvider>
@@ -17,7 +32,8 @@ export default function App() {
           screenOptions={{ headerShown: false }}
         >
           <Stack.Screen name="Onboarding" component={Onboarding} />
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Preference" component={Preference} />
         </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
