563f0d2b5a1f8c21e4fd2568a6a9f09628d66337
Apungu Raymond <apunguray@gmail.com>
2022-08-06 04:05:01 +0800

    local changes

---
+++ components/Listing/ListingCard.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListingCard = () => {
+  return (
+    <View>
+      <Text>ListingCard</Text>
+    </View>
+  )
+}
+
+export default ListingCard
