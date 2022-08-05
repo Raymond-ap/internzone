a721cffee56ed1376933898923b47de9b0fef66e
Apungu Raymond <apunguray@gmail.com>
2022-08-06 04:05:22 +0800

    local changes

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import Suggestion from "./Suggestions/Suggestion";
+import ListingCard from "./Listing/ListingCard";
+import RenderListings from './Listing/RenderListings'
+
+export {
+    Suggestion,
+    ListingCard,
+    RenderListings
+}
