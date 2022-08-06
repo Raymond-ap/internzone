8656a83b7f5feb110e42f92ba64cbd92a3e4311c
Apungu Raymond <apunguray@gmail.com>
2022-08-07 07:30:29 +0800

    render header

--- screens/Search/SearchScreen.js
+++ screens/Search/SearchScreen.js
@@ -1,12 +1,47 @@
-import { Text, View, SafeAreaView } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import React, { Component } from "react";
 import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
 
 export class SearchScreen extends Component {
+  state = {
+    search: "",
+    data: [],
+    isLoading: false,
+    error: null,
+    scrollHeight: 0,
+  };
+
   render() {
     return (
       <SafeAreaView className="bg-white" style={{ flex: 1 }}>
-        <Text>SearchScreen</Text>
+        <View
+          className={`px-3 py-3 flex flex-row items-center ${
+            this.state.scrollHeight > 0
+              ? "border-b border-gray-400 shadow-lg"
+              : ""
+          }`}
+        >
+          <TouchableOpacity
+            className="mr-2"
+            onPress={() => this.props.navigation.goBack()}
+          >
+            <Ionicons name="ios-arrow-back" size={25} color="black" />
+          </TouchableOpacity>
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Search jobs by keyword..."
+            className="w-full bg-gray-200 text-gray-800 text-base rounded-lg px-3 py-2"
+            onChangeText={(text) => this.setState({ search: text })}
+            value={this.state.search}
+          />
+        </View>
         <StatusBar style="dark" backgroundColor="#fff" translucent={false} />
       </SafeAreaView>
     );
