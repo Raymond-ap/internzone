5aea203340c53cb778a1931bbd50008494267306
Apungu Raymond <apunguray@gmail.com>
2022-08-06 21:59:08 +0800

    animated loading

--- components/AnimatedLoader.js
+++ components/AnimatedLoader.js
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import React from "react";
-// import LottieView from 'lottie-react-native'
+import LottieView from 'lottie-react-native'
 
 const AnimatedLoader = () => {
   return (
     <View style={[styles.container, StyleSheet.absoluteFillObject]}>
-        <ActivityIndicator size={"large"} color={"#fff"} />
-        {/* <LottieView source={require('../assets/images/99257-loading-gif-animation.json')} autoPlay loop /> */}
+      <LottieView
+        source={require("../assets/99297-loading-files.json")}
+        autoPlay
+        loop
+      />
     </View>
   );
 };
@@ -17,6 +20,6 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#00000061",
+    // backgroundColor: "#00000061",
   },
 });
