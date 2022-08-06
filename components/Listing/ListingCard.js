004dc78ba034df3da92edb527eea3110ba0019a7
Apungu Raymond <apunguray@gmail.com>
2022-08-06 20:06:52 +0800

    fixed card

--- components/Listing/ListingCard.js
+++ components/Listing/ListingCard.js
@@ -8,30 +8,47 @@ const ListingCard = () => {
       activeOpacity={0.8}
       className={` border px-1.5 py-3 border-gray-300 rounded-md hover:bg-gray-300 my-1 `}
     >
-      <View className="flex flex-row items-center mb-2">
-        <View className="h-10 w-10 rounded-md object-cover bg-blue-800 mr-3" />
-        <View>
-          <Text className="font-semibold text-base tracking-wider capitalize text-gray-800 ">
-            Junior frontend developer
-          </Text>
-          <Text className="text-gray-600 font-bold text-xs capitalize">
-            Accra Institute of technology
-          </Text>
+      <View className="flex flex-row justify-between mb-2">
+        <View style={{ flex: 1 }} className="flex flex-row">
+          <View className="h-10 w-10 rounded-md object-cover bg-blue-800 mr-3" />
+          <View style={{ flex: 1 }}>
+            <Text className="font-semibold text-base tracking-wider capitalize text-gray-800 ">
+              Junior frontend developer
+            </Text>
+            <Text className="text-gray-600 font-bold text-xs capitalize">
+              Accra Institute of technology
+            </Text>
+          </View>
         </View>
+        <TouchableOpacity activeOpacity={0.8}>
+          <Ionicons
+            name="bookmark-outline"
+            size={24}
+            className="text-gray-500"
+          />
+        </TouchableOpacity>
       </View>
       <View className="flex flex-row items-center">
-        <Text className="bg-gray-200 text-center hover:bg-gray-300 px-2 py-1.5 rounded-md font-bold text-xs capitalize">
+        <Text
+          style={{ flex: 1 }}
+          className="bg-gray-200 w-full text-center hover:bg-gray-300 px-2 py-1.5 rounded-md font-bold text-xs capitalize"
+        >
           Full time
         </Text>
-        <Text className="bg-gray-200 text-center mx-2 hover:bg-gray-300 px-2 py-1.5 rounded-md font-bold text-xs capitalize">
+        <Text
+          style={{ flex: 1 }}
+          className="bg-gray-200 w-full text-center mx-2 hover:bg-gray-300 px-2 py-1.5 rounded-md font-bold text-xs capitalize"
+        >
           internship
         </Text>
-        <Text className="bg-gray-200 text-center hover:bg-gray-300 px-2 py-1.5 rounded-md font-bold text-xs capitalize">
+        <Text
+          style={{ flex: 1 }}
+          className="bg-gray-200 w-full text-center hover:bg-gray-300 px-2 py-1.5 rounded-md font-bold text-xs capitalize"
+        >
           $3k/mo
         </Text>
       </View>
-      <View className="flex flex-row items-center">
-      </View>
+      <View className="flex flex-row items-center"></View>
     </TouchableOpacity>
   );
 };
