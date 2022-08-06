c0e97b157aee9d7c87341d5e21c99273c2dee852
Apungu Raymond <apunguray@gmail.com>
2022-08-06 21:59:44 +0800

    committing changes

--- screens/Details/ListingDetail.js
+++ screens/Details/ListingDetail.js
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { AnimatedLoader } from "../../components";
 
 const ListingDetail = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>ListingDetail</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white" style={{ flex: 1 }}>
+      <View className="flex flex-row justify-between px-3 py-4 z-50">
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.goBack()}
+        >
+          <Ionicons name="ios-arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        {!isLoading && (
+          <View className="flex flex-row items-center">
+            <TouchableOpacity activeOpacity={0.8}>
+              <Ionicons name="ios-bookmark-outline" size={20} color="black" />
+            </TouchableOpacity>
+            <View className="mx-1" />
+            <TouchableOpacity activeOpacity={0.8}>
+              <Ionicons name="ios-share-outline" size={20} color="black" />
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+      <StatusBar style="dark" backgroundColor="#fff" translucent={false} />
+      {isLoading && <AnimatedLoader />}
+    </SafeAreaView>
+  );
+};
 
-export default ListingDetail
+export default ListingDetail;
