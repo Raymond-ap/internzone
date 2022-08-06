245f0a23e47267f72e49fe316c627c020e31079e
Apungu Raymond <apunguray@gmail.com>
2022-08-06 21:59:23 +0800

    local changes

--- components/Suggestions/SuggestCard.js
+++ components/Suggestions/SuggestCard.js
@@ -1,10 +1,13 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const SuggestCard = () => {
+  const navigation = useNavigation();
   return (
     <TouchableOpacity
       activeOpacity={0.9}
+      onPress={() => navigation.navigate("ListingDetail")}
       className="px-2 py-2 rounded-lg shadow-sm bg-gray-900 hover:bg-gray-800 w-72 my-1 mx-2"
     >
       <View className="flex flex-row items-center justify-between">
