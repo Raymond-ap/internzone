1b4e9faed9aca8c6dc90180f2bbf757ccb627c5a
Apungu Raymond <apunguray@gmail.com>
2022-08-07 07:14:41 +0800

    local update

--- screens/index.js
+++ screens/index.js
@@ -2,10 +2,12 @@ import HomeScreen from './HomeScreen/HomeScreen'
 import Onboarding from './Onboarding'
 import Preference from './Preference/Preference'
 import ListingDetail from './Details/ListingDetail'
+import SearchScreen from './Search/SearchScreen'
 
 export {
     HomeScreen,
     Onboarding,
     Preference,
-    ListingDetail
+    ListingDetail,
+    SearchScreen
 }
