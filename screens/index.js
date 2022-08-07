d7971e425ee4d9e6f798240f0ad5abb3f1331621
Apungu Raymond <apunguray@gmail.com>
2022-08-07 22:18:15 +0800

    committing changes

--- screens/index.js
+++ screens/index.js
@@ -3,11 +3,13 @@ import Onboarding from './Onboarding'
 import Preference from './Preference/Preference'
 import ListingDetail from './Details/ListingDetail'
 import SearchScreen from './Search/SearchScreen'
+import Bookman from './Bookman/Bookman'
 
 export {
     HomeScreen,
     Onboarding,
     Preference,
     ListingDetail,
-    SearchScreen
+    SearchScreen,
+    Bookman
 }
