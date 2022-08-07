6163aeefcb601ee445d29542a1937b3d961bbf2c
Apungu Raymond <apunguray@gmail.com>
2022-08-08 02:56:35 +0800

    basic rendering

--- screens/Bookman/Bookman.js
+++ screens/Bookman/Bookman.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  ScrollView,
+  RefreshControl,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { AnimatedLoader } from "../../components";
+import { AnimatedLoader, ListingCard } from "../../components";
 import { StatusBar } from "expo-status-bar";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const wait = (timeout) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+};
 
 const Bookman = () => {
   const [scrollHeight, setscrollHeight] = React.useState(0);
@@ -13,14 +26,52 @@ const Bookman = () => {
 
   const getBookman = async () => {};
 
+  const onRefresh = async () => {
+    setIsLoading(true);
+    await wait(2000);
+    getBookman();
+  };
+
   React.useEffect(() => {
     getBookman();
-  }, []);
+  }, [isLoading]);
 
   return (
     <SafeAreaView style={{ flex: 1 }} className="bg-white">
       <Header scrollHeight={scrollHeight} />
+      <View className="px-3 py-3">
+        {listing.length > 0 ? (
+          <FlatList
+            data={listing}
+            renderItem={({ item }) => (
+              <ListingCard listing={item} bookmanArray={bookmanID} />
+            )}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            onScroll={(e) => {
+              setscrollHeight(e.nativeEvent.contentOffset.y);
+            }}
+            refreshControl={
+              <RefreshControl refreshing={isLoading} onRefresh={onRefresh} />
+            }
+          />
+        ) : (
+          <ScrollView
+            refreshControl={
+              <RefreshControl refreshing={isLoading} onRefresh={onRefresh} />
+            }
+          >
+            <View className="flex items-center justify-center h-full">
+              <View className="h-20 w-20 object-cover bg-blue-600 mb-3" />
+              <Text className="text-gray-600 text-base">
+                No saved jobs found. Save a job to see it here.
+              </Text>
+            </View>
+          </ScrollView>
+        )}
+      </View>
       <StatusBar style="dark" backgroundColor="#fff" translucent={false} />
+      {isLoading && <AnimatedLoader />}
     </SafeAreaView>
   );
 };
