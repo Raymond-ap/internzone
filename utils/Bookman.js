8cd362ff592f321e419f8ede2519a652bc9b8b6e
Apungu Raymond <apunguray@gmail.com>
2022-08-07 20:09:32 +0800

    local update

--- utils/Bookman.js
+++ utils/Bookman.js
@@ -2,5 +2,5 @@ import React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const ToogleBookman = async (docId) => {
-  await AsyncStorage.removeItem("bookman");
+ 
 };
