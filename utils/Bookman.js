36a832a90fb396b557d1bc7db94070f24c2fadc5
Apungu Raymond <apunguray@gmail.com>
2022-08-08 18:36:05 +0800

    committing changes

--- utils/Bookman.js
+++ utils/Bookman.js
@@ -2,6 +2,20 @@ import React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const ToogleBookman = async (docId) => { 
-  await AsyncStorage.removeItem("bookman");
- 
+  const bookman = await AsyncStorage.getItem("bookman");
+  console.log("bookman", bookman);
+  if (bookman) {
+    const bookmanArray = JSON.parse(bookman);
+    const index = bookmanArray.indexOf(docId);
+    if (index > -1) {
+      bookmanArray.splice(index, 1);
+    } else {
+      bookmanArray.push(docId);
+    }
+    await AsyncStorage.setItem("bookman", JSON.stringify(bookmanArray));
+  } else {
+    await AsyncStorage.setItem("bookman", JSON.stringify([docId]));
+  } 
 };
+
+//  await AsyncStorage.removeItem("bookman");
